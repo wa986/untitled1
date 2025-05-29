3e682365d03999e4d8b77c80bb855ad76f61f502
yamamotonatuki <241058`@std.hi-joho.ac.jp>
2025-05-29 17:04:34 +0900

    1完成

--- src/reviews/reviews1/Review4.java
+++ src/reviews/reviews1/Review4.java
@@ -6,14 +6,14 @@ public class Review4 {
         int number = 5;
         String str = 88 + "88"; //②
         final double PI = 3.141592; //③
-//PI = 3.14; //④再代入禁止
-//float circ = 3.14; //⑤doubleからfloatに暗黙型型変換で代入されない
-        long a = 10000000000L; //⑥int型のリテラル超えるからlong　long型のリテラルはlが必要
-//boolean flg = "true"; //⑦
+//PI = 3.14; //再代入禁止
+//float circ = 3.14; //doubleからfloatに暗黙型型変換で代入されない
+        long a = 10000000000L; //int型のリテラル超えるからlong　long型のリテラルはlが必要
+//boolean flg = "true";
         boolean flg = true;
-        byte b = 35; //⑧
-        int num = 0x7F5; //⑨
-        //char c = "a"; //⑩
+        byte b = 35;
+        int num = 0x7F5;
+        //char c = "a";
         char c = 'a';
     }
 }
