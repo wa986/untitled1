12c3de8da13796b8a0da250820d15bea3332b75b
yamamotonatuki <241058`@std.hi-joho.ac.jp>
2025-06-02 12:07:16 +0900

    加点用課題

--- src/textbook_practice/chapter6/Practice6_3/Main.java
+++ src/textbook_practice/chapter6/Practice6_3/Main.java
@@ -1,11 +1,9 @@
 package textbook_practice.chapter6.Practice6_3;
 
-import textbook_practice.chapter6.Practice6_1.comment.Zenhan;
-
 public class Main {
     public static void main(String[] args) throws Exception {
-        Zenhan.doWarusa(); // 前半
-        Zenhan.doTogame(); // 前半
+        textbook_practice.chapter6.Practice6_1.comment.Zenhan.doWarusa(); // 前半
+        textbook_practice.chapter6.Practice6_1.comment.Zenhan.doTogame(); // 前半
         textbook_practice.chapter6.Practice6_1.comment.Kouhan.callDeae(); // 後半
         textbook_practice.chapter6.Practice6_1.comment.Kouhan.showMondokoro(); // 後半
     }
