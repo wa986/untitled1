e5ceff580bd25cfc54034b38bc420710ed4f78df
yamamotonatuki <241058`@std.hi-joho.ac.jp>
2025-06-02 12:13:48 +0900

    なんか

--- src/textbook_practice/chapter6/Practice6_3/comment/Kouhan.java
+++ src/textbook_practice/chapter6/Practice6_3/comment/Kouhan.java
@@ -10,7 +10,7 @@ public class Kouhan {
     public static void showMondokoro() throws Exception {
         System.out.println("飛車さん、角さん。もういいでしょう。");
         System.out.println("この紋所が目にはいらぬか！");
-        Thread.sleep(3000); // この行を追加
+        Thread.sleep(3000); // この行を追加　分からん
         Zenhan.doTogame();     // もう一度、とがめる
     }
-}//分からん
+}
